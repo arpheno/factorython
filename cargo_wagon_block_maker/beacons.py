f6b79422effa0cadb6911a35cf855627d41468ab
swozny <arpheno@gmail.com>
2023-10-05 13:56:43 +0200

    Adds beacons and lights

---
+++ cargo_wagon_block_maker/beacons.py
@@ -0,0 +1,26 @@
+from draftsman.classes.blueprint import Blueprint
+from draftsman.classes.group import Group
+from draftsman.prototypes.beacon import Beacon
+
+from cargo_wagon_block_maker.assembling_machines_group import AssemblingMachinesGroup
+from cargo_wagon_block_maker.bbmm import BlueprintMakerModule
+
+
+class Beacons(BlueprintMakerModule):
+    def build(self, assembling_machines: AssemblingMachinesGroup, output: str):
+        g = Group()
+        for top_ref in assembling_machines.top_row[2::4]:
+            g.entities.append(
+            Beacon(**{'name': 'beacon',
+                      'position':{'x':top_ref.global_position['x']-1,
+                                  'y':top_ref.global_position['y']-5},
+                      'items': {'speed-module-2':8},
+                      }))
+        for bot_ref in assembling_machines.bottom_row[2::4]:
+            g.entities.append(
+            Beacon(**{'name': 'beacon',
+                      'position':{'x':bot_ref.global_position['x']-1,
+                                  'y':bot_ref.global_position['y']+4},
+                      'items': {'speed-module-2':8},
+                      }))
+        return g
---
+++ cargo_wagon_block_maker/lights.py
@@ -0,0 +1,34 @@
+from draftsman.classes.group import Group
+
+from cargo_wagon_block_maker.assembling_machines_group import AssemblingMachinesGroup
+from cargo_wagon_block_maker.bbmm import BlueprintMakerModule
+
+def lights():
+    g=Group(entities=[
+        {
+            "name": "small-lamp",
+            "position": {
+                "x": 0,
+                "y": -4
+            }
+        },
+        {
+            "name": "small-lamp",
+            "position": {
+                "x": 0,
+                "y": 12
+            }
+        },
+    ])
+    return g
+
+class Lights(BlueprintMakerModule):
+
+    def build(self, assembling_machines: AssemblingMachinesGroup, output: str):
+        g = Group()
+        for machine in assembling_machines.top_row[1::4]:
+            p = lights()
+            p.translate(machine.global_position['x'], machine.global_position['y'])
+            g.entities.append(p)
+        return g
+        pass
