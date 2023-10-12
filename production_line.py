7c92c0f2ac2e9e500f179c4efbc518cb7886462e
swozny <arpheno@gmail.com>
2023-10-12 16:55:58 +0200

    Production lines now accept a duration so you can print how much it consumes in X seconds

--- production_line.py
+++ production_line.py
@@ -9,14 +9,17 @@ class ProductionLine:
         self.production_sites = production_sites
         self.net_production = net_production
 
-    def print(self, ):
+    def print(self, duration=1):
 
         if self.status != "Optimal":
             print("Production line is not optimal")
         else:
             print("Production Sites")
             for recipe_name,production_site in self.production_sites.items():
-                print(f'{recipe_name}: {production_site.quantity:.2f} x {production_site.building.name}')
+                if 'ltn' in recipe_name:
+                    print(f'{recipe_name}: {production_site.quantity*duration:.2f} x {production_site.building.name} (input)')
+                else:
+                    print(f'{recipe_name}: {production_site.quantity:.2f} x {production_site.building.name}')
 
             print("Net Production:")
             for item, production in self.net_production.items():
