6f4f69d6dff93fdb5b4851ab31f363f5cf059887
swozny <arpheno@gmail.com>
2023-10-21 15:54:02 +0200

    - Barrelers were flipping directions unnecessarily

--- space/liquids.py
+++ space/liquids.py
@@ -1,56 +1,61 @@
+import math
+import warnings
+
 from draftsman.classes.blueprint import Blueprint
 from draftsman.classes.group import Group
 from draftsman.constants import Direction
 from draftsman.data.entities import assembling_machines
+from draftsman.error import InvalidRecipeError
 from draftsman.prototypes.assembling_machine import AssemblingMachine
 from draftsman.prototypes.inserter import Inserter
 
 print(assembling_machines)
 
 
-def barreler(mode, fluid, amount):
-    g = Group()
+def barreler(mode, fluid, amount, provider_chest_name="logistic-chest-active-provider"):
     request_filters = [
-        {"index": 1, "name": f"{fluid}-barrel", "count": amount // 50 + 1} if mode=='empty' else
-        {"index": 1, "name": f"empty-barrel", "count": amount // 50 + 1}
+        {"index": 1, "name": f"{fluid}-barrel", "count": math.ceil(amount // 50 + 1)} if mode == 'empty' else
+        {"index": 1, "name": f"empty-barrel", "count": math.ceil(amount // 50 + 1)}
     ]
-    entities = ents(fluid, mode, request_filters)
-    for entity in entities:
-        g.entities.append(**entity)
-    return g
-
-def ents(fluid, mode, request_filters,d1=Direction.SOUTH,d2=Direction.NORTH):
     entities = [
         {
             "name": "fast-inserter",
             "id": "output_inserter",
             "position": {"x": 1, "y": 2},
-            "direction": d1 if mode == "fill" else d2,
+            "direction": Direction.NORTH,
         },
         {
             "name": "fast-inserter",
             "id": "input_inserter",
             "position": {"x": -1, "y": 2},
-            "direction": d1 if mode == "empty" else d2,
+            "direction": Direction.SOUTH,
         },
         {
             "name": "se-space-assembling-machine",
             "recipe": f"{mode}-{fluid}-barrel",
             "id": "machine",
             "position": {"x": 0, "y": 0},
-            "direction": d1 if mode=='empty' else d2,
+            "direction": Direction.SOUTH if mode == 'empty' else Direction.NORTH
         },
         {
             "name": "logistic-chest-requester",
-            'id':'requester_chest',
+            'id': 'requester_chest',
             "position": (-1, 3),
             "request_filters": request_filters,
         },
-        {"name": "logistic-chest-active-provider",
-         'id':'provider_chest',"position": (1, 3), "bar": 1},
-        {"name": "medium-electric-pole", 'id':'power_pole',"position": (0, 3)},
-    ]
-    return entities
+        {"name": provider_chest_name,
+         'id': 'provider_chest',
+         "position": (1, 3), "bar": 1},
+            {"name": "medium-electric-pole", 'id': 'power_pole', "position": (0, 3)},
+        ]
+    try:
+        g = Group()
+        for entity in entities:
+            g.entities.append(**entity)
+    except InvalidRecipeError:
+        g = Group()
+        warnings.warn(f'Cannot barrel {fluid}')
+    return g
 
 
 if __name__ == "__main__":
