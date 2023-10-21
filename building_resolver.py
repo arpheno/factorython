5d090f16b8323580d3cc061377e66f16105dda45
swozny <arpheno@gmail.com>
2023-10-21 15:53:07 +0200

    - Fake machines should also emulate module specifications

--- building_resolver.py
+++ building_resolver.py
@@ -26,6 +26,7 @@ class FakeAssemblyMachine:
     def __init__(self, name, crafting_speed=1):
         self.name = name
         self.crafting_speed = crafting_speed
+        self.module_specification = None
 
 
 def find_fastest_assembler(recipe, crafting_categories):
